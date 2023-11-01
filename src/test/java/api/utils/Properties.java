4bb405950fde0d4023819b3c84aa21c8f6bd45e2
Priset <emanuel.1kll@gmail.com>
2023-11-01 11:44:04 -0400

    Commit Inicial

---
+++ src/test/java/api/utils/Properties.java
@@ -0,0 +1,9 @@
+package api.utils;
+
+public class Properties {
+
+    public static String host = "https://todo.ly/";
+    public static String user = "priset@gmail.com";
+    public static String pwd = "priset1234";
+
+}
